703379affa4b5e677aed670a12140133a5889c2d
Shobhit Nigam <50029224+shobhit-nigam@users.noreply.github.com>
2021-09-24 09:23:32 +0530

    Add files via upload

---
+++ day3/modules/colours.py
@@ -0,0 +1,23 @@
+def white():
+    print("pure white")
+    
+def black():
+    print("elegant black")
+    
+def blue():
+    print("cool blue")
+    
+
+def greece():
+    print("greek flag needs")
+    white()
+    blue()
+
+
+def estonia():
+    print("estonia flag needs")
+    white()
+    blue()
+    black()
+    
+lista = [20, 30, 40]

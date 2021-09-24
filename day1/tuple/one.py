a6eed982734d25dd7c13ab4283cc1ad563851448
Shobhit Nigam <50029224+shobhit-nigam@users.noreply.github.com>
2021-09-23 22:27:58 -0600

    Add files via upload

---
+++ day1/tuple/one.py
@@ -0,0 +1,7 @@
+listnum = [12, 67, 85, 23, 96, 50, 67, 23, 33]
+
+print(type(listnum))
+
+listnum = (12, 67, 85, 23, 96, 50, 67, 23, 33)
+
+print(type(listnum))
---
+++ day1/tuple/two.py
@@ -0,0 +1,6 @@
+listnum = (12, 67, 85, 23, 96, 50, 67, 23, 33)
+
+print(type(listnum))
+listnum.insert(2, 50)
+
+print(type(listnum))
